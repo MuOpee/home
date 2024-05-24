bda863cf6563044de18fb4bf6b5c56e261006586
Mujahid Hossain <opee.home@outlook.com>
2024-05-24 17:51:23 +0600

    Update figure.js

--- figure.js
+++ figure.js
@@ -1,3 +1,8 @@
-let date = new Date();
-let time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-document.getElementById("time").innerText = time;
+// currentDate.js
+const currentDateAndTime = () => {
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    return now.toLocaleString('en-US', options);
+}
+
+console.log(currentDateAndTime());
